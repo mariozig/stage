69fbd4f6d3775478b0574cf30f7ec399b75741df
Mario Zigliotto <mariozig@gmail.com>
2018-08-26 10:07:45 -0700

    Reorder columns

--- app/dashboards/gallery_dashboard.rb
+++ app/dashboards/gallery_dashboard.rb
@@ -23,10 +23,10 @@ class GalleryDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :submission,
     :id,
     :title,
     :description,
+    :submission
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
