d8437c1fabc679cff34b2a48b32f29583039fd19
Mario Zigliotto <mariozig@gmail.com>
2018-10-08 22:19:06 -0700

    Reintegrate pagination with updated styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @pagy, @galleries = pagy(Gallery.all, items: 40)
+    @pagy, @galleries = pagy(Gallery.all, items: 2)
   end
 
   def terms; end
