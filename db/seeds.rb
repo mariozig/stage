b55311c0443ccc9fe4a61a88f1f6ed026747e458
Mario Zigliotto <mariozig@gmail.com>
2018-10-14 14:16:34 -0700

    Seed an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
+User.create!(email: 'admin@example.com', password: 'Password123!', name: 'The Admin', admin: true)
 Category.create!(name: 'Uncategorized', description: 'Stuff that still needs a proper home.')
 ReportReason.create!(content: 'Other', position: 0)
